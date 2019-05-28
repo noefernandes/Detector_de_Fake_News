19b5268d49b819e7037230d60e47fe19666db935
Noé Fernandes <noefernandes@outlook.com>
2019-05-28 16:09:51 -0300

    Lendo arquivo

--- src/br/ufrn/imd/controle/LeitorArquivo.java
+++ src/br/ufrn/imd/controle/LeitorArquivo.java
@@ -1,25 +1,34 @@
 package br.ufrn.imd.controle;
 
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class LeitorArquivo {
 	
+	protected ArrayList<String> linhas;
+	
+	public LeitorArquivo() {
+		linhas = new ArrayList<String>();
+	}
+	
 	public void load() throws FileNotFoundException {
-		Scanner scanner = new Scanner(new File("boatos.csv"));
+		Scanner scanner = new Scanner(new FileReader("boatos.csv"));
 	    
-	    //Set the delimiter used in file
-	    scanner.useDelimiter(",");
-	     
-	    //Get all tokens and store them in some data structure
-	    //I am just printing them
+	    scanner.useDelimiter(";");
+	    
+	    int count = 0;
+	
 	    while (scanner.hasNext())
 	    {
-	        System.out.print(scanner.next() + "|");
+	    	//System.out.println(scanner.next());
+	    	count = count + 1 ;
 	    }
 	     
-	    //Do not forget to close the scanner 
+	    System.out.println("value:" + count);
 	    scanner.close();
+	    
 	}
 }
